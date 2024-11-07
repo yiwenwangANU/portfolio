b438768419250913e939e5658af3d51552eee7cc
admin01 <paradoxraphael@gmai.com>
2024-11-08 10:56:46 +1100

    add frame-motion and testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,36 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
+import { motion } from "framer-motion";
 function App() {
-  const [count, setCount] = useState(0);
+  const variants = {
+    hiddenLeft: { opacity: 0, x: -50 },
+    hiddenRight: { opacity: 0, x: 50 },
+    visible: { opacity: 1, x: 0 },
+  };
 
   return (
     <>
+      <p>dummy content</p>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <motion.div
+          variants={variants}
+          initial="hiddenLeft"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 1 }}
+        >
+          123
+        </motion.div>
+
+        <motion.div
+          variants={variants}
+          initial="hiddenRight"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 1 }}
+        >
+          456
+        </motion.div>
       </div>
-      <h1>Vite + React</h1>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="font-bold bg-red-700">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
