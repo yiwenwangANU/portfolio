f346ebc2a63d0dc47a8371385cb6f5220739f549
admin01 <paradoxraphael@gmai.com>
2024-11-16 11:27:43 +1100

    add new component frontPage

---
+++ src/components/FrontPage.jsx
@@ -0,0 +1,22 @@
+import Button from "./Button";
+
+function FrontPage() {
+  return (
+    <div className="bg-[url('code-background-2.JPG')] bg-cover bg-center h-screen w-full text-white px-32 py-32">
+      <h1 className=" text-7xl font-bold">
+        Hi !
+        <br /> I am Yiwen Wang
+      </h1>
+      <p className="text-gray-300 pt-4">
+        Need a talented full-stack developer? Let me turn your ideas into
+        reality.
+      </p>
+      <div className="pt-5 flex flex-row gap-4">
+        <Button variation="secondary">Hire me</Button>
+        <Button variation="secondary">Download resume</Button>
+      </div>
+    </div>
+  );
+}
+
+export default FrontPage;
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import Button from "./Button";
+import FrontPage from "./FrontPage";
 
 const variants = {
   hiddenLeft: { opacity: 0, x: -50 },
@@ -10,20 +10,7 @@ const variants = {
 function Portfolio() {
   return (
     <div>
-      <div className="bg-[url('code-background-2.JPG')] bg-cover bg-center h-screen w-full text-white px-32 py-32">
-        <h1 className=" text-7xl font-bold">
-          Hi !
-          <br /> I am Yiwen Wang
-        </h1>
-        <p className="text-gray-300 pt-4">
-          Need a talented full-stack developer? Let me turn your ideas into
-          reality.
-        </p>
-        <div className="pt-5 flex flex-row gap-4">
-          <Button variation="secondary">Hire me</Button>
-          <Button variation="secondary">Download resume</Button>
-        </div>
-      </div>
+      <FrontPage />
       <div>
         <motion.div
           variants={variants}
