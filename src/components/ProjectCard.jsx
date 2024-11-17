51ad7af95d62b5eae9304d13dd644455125d4f4f
admin01 <paradoxraphael@gmai.com>
2024-11-17 15:09:20 +1100

    add project card to project page

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,26 +2,28 @@ import { NavLink } from "react-router-dom";
 
 function ProjectCard({ image, title, description, tabs, url, gitUrl }) {
   return (
-    <div className="w-60 h-80 rounded-lg border border-white">
-      <img
-        src={image}
-        alt="Preview image not available"
-        className="w-full h-1/2"
-      />
-      <div className="px-3 py-2">
-        <div>{title}</div>
-        <div className="text-gray-50">{description}</div>
-        <div className="flex flex-row gap-2">
-          {tabs.map((item, index) => (
-            <div key={index} className="border border-purple-700 rounded-md">
-              {item}
-            </div>
-          ))}
+    <NavLink to={url}>
+      <div className="w-2/5 h-[455px] rounded-xl border border-white">
+        <img
+          src={image}
+          alt="Preview image not available"
+          className="w-full h-1/2 rounded-xl"
+        />
+        <div className="px-3 py-2">
+          <div>{title}</div>
+          <div className="text-gray-50">{description}</div>
+          <div className="flex flex-row gap-2">
+            {tabs.map((item, index) => (
+              <div key={index} className="border border-purple-700 rounded-md">
+                {item}
+              </div>
+            ))}
+          </div>
+          <NavLink to={url}>web</NavLink>
+          <NavLink to={gitUrl}>git</NavLink>
         </div>
-        <NavLink to={url}>web</NavLink>
-        <NavLink to={gitUrl}>git</NavLink>
       </div>
-    </div>
+    </NavLink>
   );
 }
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,18 @@
+import ProjectCard from "./ProjectCard";
+
+const projectsInfo = [
+  {
+    image: "wild-oasis-admin.jpg",
+    title: "Wild Oasis - Admin Backend",
+    description: "Some description",
+    tabs: [0, 1],
+    url: "https://wild-oasis-v2-hr4ywrovh-yiwens-projects-7b8f585a.vercel.app",
+    gitUrl: "https://github.com/yiwenwangANU/wild-oasis-v2",
+  },
+];
 function Projects() {
   return (
-    <div className="px-72 pt-20">
+    <div className="px-96 pt-20 text-white">
       <div className="text-white text-3xl font-bold">Recent Projects</div>
       <div className="text-gray-300 text-lg py-3">
         Here are some of the projects I&apos;ve worked on. Check out{" "}
@@ -13,7 +25,19 @@ function Projects() {
         </a>{" "}
         for more.
       </div>
-      <div>projects</div>
+      <div className="py-6">
+        {projectsInfo.map((item) => (
+          <ProjectCard
+            image={item.image}
+            title={item.title}
+            description={item.description}
+            tabs={item.tabs}
+            url={item.url}
+            gitUrl={item.gitUrl}
+            key={item.title}
+          />
+        ))}
+      </div>
     </div>
   );
 }
