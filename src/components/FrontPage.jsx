bebf6b34719d527ab0a7656672370c5a8e7745db
admin01 <paradoxraphael@gmai.com>
2024-11-17 13:18:02 +1100

    add animation to frontpage with motion

--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -1,20 +1,35 @@
 import Button from "./Button";
-
+import { motion } from "framer-motion";
 function FrontPage() {
   return (
     <div className="bg-[url('code-background-3.jpg')] bg-cover bg-center h-screen w-full text-white px-72 py-40">
-      <h1 className=" text-9xl font-bold">
+      <motion.h1
+        className=" text-9xl font-bold"
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+      >
         Hi !
         <br /> I am Yiwen Wang
-      </h1>
-      <p className="text-gray-300 py-6 text-xl">
+      </motion.h1>
+      <motion.p
+        className="text-gray-300 py-6 text-xl"
+        initial={{ opacity: 0, x: 150 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1, delay: 0.3 }}
+      >
         Need a talented full-stack developer? Let me turn your ideas into
         reality.
-      </p>
-      <div className="flex flex-row gap-4">
+      </motion.p>
+      <motion.div
+        className="flex flex-row gap-4"
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, delay: 0.6 }}
+      >
         <Button variation="secondary">Hire me</Button>
         <Button variation="secondary">Download resume</Button>
-      </div>
+      </motion.div>
     </div>
   );
 }
