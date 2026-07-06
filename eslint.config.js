34de991dcdb892299b6d905746235e19afac3743
Yiwen Wang <wangyiwenau@gmail.com>
2026-07-06 10:01:26 +1000

    chore: update packages, install eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,39 +1,34 @@
 import js from "@eslint/js";
 import globals from "globals";
-import react from "eslint-plugin-react";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig } from "eslint/config";
 
-export default [
+export default defineConfig([
   { ignores: ["dist"] },
   {
-    files: ["**/*.{js,jsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: "latest",
-        ecmaFeatures: { jsx: true },
-        sourceType: "module",
-      },
-    },
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js, "react-hooks": reactHooks, "react-refresh": reactRefresh },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
     settings: { react: { version: "18.3" } },
-    plugins: {
-      react,
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
     rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
-      "react/jsx-no-target-blank": "off",
-      "react/prop-types": "off",
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
     },
   },
-];
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
+  {
+    rules: {
+      "react/prop-types": "off",
+      "react/jsx-no-target-blank": "off",
+    },
+  },
+]);
